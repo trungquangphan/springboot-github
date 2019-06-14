959f8e222c08f93ba5bc5ee400ba5996ee1e6124
Trung Phan <quangtrung296@gmail.com>
2019-06-14 13:13:53 +0700

    Initial commit

---
+++ src/main/java/vn/codegymdanang/demo/controller/HomeController.java
@@ -0,0 +1,10 @@
+package vn.codegymdanang.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    public String home(){
+        return "index";
+    }
+}
